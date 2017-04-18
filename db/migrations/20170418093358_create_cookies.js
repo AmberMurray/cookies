c4df5545516c90d8a893b6e55d83143f5bd518d0
Amber Murray <amber.applebee@gmail.com>
2017-04-18 09:48:07 -0700

    created migration and seeds

---
+++ db/migrations/20170418093358_create_cookies.js
@@ -0,0 +1,17 @@
+
+'use strict'
+
+exports.up = function(knex) {
+  return knex.schema.createTable('cookies', table => {
+    table.increments()
+    table.string('name').notNullable().defaultTo('')
+    table.string('mouthfeel').notNullable().defaultTo('')
+    table.integer('num_ingredients').notNullable().defaultTo(0)
+    table.boolean('delicious').notNullable().defaultTo('')
+    table.timestamps(true, true)
+  })
+}
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('addresses')
+}
---
+++ db/seeds/1_cookie_seed_data.js
@@ -0,0 +1,34 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('cookies').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('cookies').insert([
+        {
+          name: 'chocolate chip',
+          mouthfeel: 'chewy',
+          num_ingredients: '7',
+          delicious: true
+        },
+        {
+          name: 'double chocolate chip',
+          mouthfeel: 'cake-like',
+          num_ingredients: '8',
+          delicious: true
+        },
+        {
+          name: 'sugar',
+          mouthfeel: 'soft',
+          num_ingredients: '4',
+          delicious: true
+        },
+        {
+          name: 'dog treat',
+          mouthfeel: 'crunchy',
+          num_ingredients: '1',
+          delicious: false
+        }
+      ]);
+    });
+};
